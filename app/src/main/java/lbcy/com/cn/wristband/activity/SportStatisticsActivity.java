98bb055cdaf03e1b4ae39ed5a980c2eeb14bd04c
LiuYingJie329 <30766728+LiuYingJie329@users.noreply.github.com>
2017-08-31 14:16:26 +0800

    原生添加新界面

---
+++ app/src/main/java/lbcy/com/cn/wristband/activity/SportStatisticsActivity.java
@@ -0,0 +1,15 @@
+package lbcy.com.cn.wristband.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lbcy.com.cn.wristband.R;
+
+public class SportStatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sport_statistics);
+    }
+}
